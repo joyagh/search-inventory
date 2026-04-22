8df336f4c29deee4df4b36f94bde1e9fa9f43e51
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-04-22 15:41:42 -0400

    added inventory

--- src/main/java/com/pluralsight/StoreApp.java
+++ src/main/java/com/pluralsight/StoreApp.java
@@ -17,6 +17,12 @@ public class StoreApp {
 // this method loads product objects into inventory
 // and its details are not shown
         inventory.add(new Product(1234, "Hammer",19.49));
+        inventory.add(new Product(2345, "Box of Nails",9.29));
+        inventory.add(new Product(9154, "9-in-1 Ratcheting Screwdriver",24.97));
+        inventory.add(new Product(1648, "10 Crescent Wrench",15.95));
+        inventory.add(new Product(9876, "Mechanics Tool Set",119.00));
+        inventory.add(new Product(1212, "Spring Clamp",.99));
+        inventory.add(new Product(3434, "Large Trigger Clamps",36.97));
         return inventory;
     }
 }
